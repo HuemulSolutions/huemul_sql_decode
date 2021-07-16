7081dc62896611864b4a81af7d1209c0ddca9e2e
Developer Code from HuemulSolutions <developer.code@huemulsolutions.com>
2021-07-16 13:49:27 -0400

    UPD mejoras scala

--- src/main/scala/com/huemulsolutions/bigdata/sql_decode/huemul_sql_columns.scala
+++ src/main/scala/com/huemulsolutions/bigdata/sql_decode/huemul_sql_columns.scala
@@ -3,8 +3,8 @@ package com.huemulsolutions.bigdata.sql_decode
 import scala.collection.mutable._
 
 class huemul_sql_columns extends Serializable {
-  var column_name: String = null
-  var column_sql: String = null
+  var column_name: String = _
+  var column_sql: String = _
   var sql_pos_start: Int = 0
   var sql_pos_end: Int = 0
   var column_origin: ArrayBuffer[huemul_sql_columns_origin] = new ArrayBuffer[huemul_sql_columns_origin]()
--- src/main/scala/com/huemulsolutions/bigdata/sql_decode/huemul_sql_tables.scala
+++ src/main/scala/com/huemulsolutions/bigdata/sql_decode/huemul_sql_tables.scala
@@ -4,7 +4,7 @@ package com.huemulsolutions.bigdata.sql_decode
  * Save "database.table alias"
  */
 class huemul_sql_tables extends Serializable {
-  var table_name: String = null
-  var database_name: String = null
-  var tableAlias_name: String = null
+  var table_name: String = _
+  var database_name: String = _
+  var tableAlias_name: String = _
 }
